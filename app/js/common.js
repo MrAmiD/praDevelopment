4ceab776f7d3b97763216105fb3dc984ad11cf06
MrAmiD <nadymov2910@gmail.com>
2018-06-01 14:35:52 +0500

    done

--- app/js/common.js
+++ app/js/common.js
@@ -1,7 +1,7 @@
 function scrollAnimate(selector){
     var absOffsetTop = selector.offset().top - $(window).height();
     var scrollWin = $(window).scrollTop();
-    if (scrollWin >= absOffsetTop - 100) {
+    if (scrollWin >= absOffsetTop - 100 && $(window).width() >= 768) {
         //console.log('addClass animate');
         selector.addClass('animate');
     } else {
